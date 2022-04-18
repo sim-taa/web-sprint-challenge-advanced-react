21ba84f67a186abb68d0049c43fb39f1cdabba9f
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-04-18 15:00:55 -0700

    fixes messages to update based on state. no longer hard-coded.

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -112,8 +112,8 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           {this.state.matrix.flatMap((x) => x)
